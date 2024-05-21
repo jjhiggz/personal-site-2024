28fb9cc076187f93d8f61f639502e6c290778835
Jon Higger <jonathan.higger@gmail.com>
2024-05-21 07:25:36 -0600

    fixed stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { twcx } from "@/utils/tailwind-helpers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark text-white">
+      <body className={twcx(inter.className, "dark")}>{children}</body>
     </html>
   );
 }
